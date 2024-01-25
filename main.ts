c200db0771dbbc5cabea7f513fc00f5276fc75ad
Yuki <github@risaton.net>
2024-01-25 09:38:33 +0900

    commit

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import * as process from "process";
-
+import * as line from '@line/bot-sdk'
 console.log('Hello')
 
 // @ts-ignore
@@ -11,5 +11,12 @@ require('dotenv').config()
 // console.log(process.env.CHANNEL_SECRET.toString())
 const PORT = process.env.PORT || 3000
 const channel_secret = process.env.CHANNEL_SECRET
+const channel_access_token = process.env.CHANNEL_ACCESS_TOKEN
+
+// @ts-ignore
+// @ts-ignore
+new line.messagingApi.MessagingApiClient({
+    channelAccessToken: channel_access_token,
 
 
+}
